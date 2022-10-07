19d455e194e696b9c2a12c17c88d406609a95ecd
Marek Kędzia <marek.kedzia@arcsoftware.it>
2022-10-07 13:04:37 +0200

    zad1

---
+++ tep/zad1/zad1.h
@@ -0,0 +1,17 @@
+//
+// Created by marek on 06.10.2022.
+//
+
+#ifndef SEMIII_ZAD1_H
+#define SEMIII_ZAD1_H
+
+#include <iostream>
+
+const int DEFAULT_TABLE_FILLER = 34;
+const std::string SPLIT_SIGN = ".";
+
+class zad1 {
+};
+
+
+#endif //SEMIII_ZAD1_H
